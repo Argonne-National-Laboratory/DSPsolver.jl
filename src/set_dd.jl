4a8fe5ad8a4bdc89b6d3f553dc273cb648d6ad01
Kibaek Kim <kibaek.k@gmail.com>
2015-11-06 11:04:17 -0600

    Update set_dd.jl

--- src/set_dd.jl
+++ src/set_dd.jl
@@ -1,8 +1,6 @@
 function setDdProcIdxSet(scenarios::Array{Int,1})
 	# num = convert(Cint, length(scenarios));
 	# scenarios = convert(Vector{Cint}, scenarios) - 1;
-	println(length(scenarios));
-	println(scenarios-1);
 	setIntPtrParam("DD/ARR_PROC_IDX", length(scenarios), scenarios-1);
 	# @dsp_ccall("setDdProcIdxSet", Void, (Ptr{Void}, Cint, Ptr{Cint}), env.p, num, scenarios)
 end
