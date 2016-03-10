56bddc224b6af22f83f9349fa29e25ec5135e27a
Kibaek Kim <kibaek.k@gmail.com>
2016-03-10 09:06:09 -0600

    Update get_dd.jl

--- src/get_dd.jl
+++ src/get_dd.jl
@@ -1,79 +1,50 @@
-function getDdNumInfeasSolutions()
-	return @dsp_ccall("getDdNumInfeasSolutions", Cint, (Ptr{Void},), env.p)
+# Get functions for dual decomposition
+# TODO: implement getDdMasterPrimalSolutions
+
+# number of infeasible solutions evaluated
+# number of master problem solved
+for (func,name) in [(:getDdNumInfeasSolutions,"getDdNumInfeasSolutions"),
+		    (:getDdNumMasterSolved,"getDdNumMasterSolved"),
+		    (:getDdNumSubproblemSolved,"getDdNumSubproblemSolved")]
+	@eval begin
+		function $func()
+			return @dsp_ccall($name, Cint, (Ptr{Void},), env.p)
+		end
+	end
 end
 
-function getDdIterTime()
-	num = @dsp_ccall("getDdIterTimeSize", Cint, (Ptr{Void},), env.p)
-	time = Array(Cdouble, num)
-	@dsp_ccall("getDdIterTime", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, time)
-	return time
+# master solution cpu time per iteration
+# master solution wall time per iteration
+# history of master primal objective values
+# history of master dual objective values
+for (func,name) in [(:getDdMasterCpuTimes,"getDdMasterCpuTimes"),
+		    (:getDdMasterWallTimes,"getDdMasterWallTimes"),
+		    (:getDdMasterPrimalBounds,"getDdMasterPrimalBounds"),
+		    (:getDdMasterDualBounds,"getDdMasterDualBounds")]
+	@eval begin
+		function $func()
+			size = getDdNumMasterSolved()
+			val = Array(Cdouble, size)
+			@dsp_ccall($name, Void, (Ptr{Void}, Ptr{Cdouble}), env.p, val)
+			return val
+		end
+	end
 end
 
-function getDdMasterTime()
-	num = @dsp_ccall("getDdMasterTimeSize", Cint, (Ptr{Void},), env.p)
-	time = Array(Cdouble, num)
-	@dsp_ccall("getDdMasterTime", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, time)
-	return time
+# subproblem solution cpu time per iteration
+# subproblem solution wall time per iteration
+# history of subproblem primal objective values
+# history of subproblem dual objective values
+for (func,name) in [(:getDdSubproblemCpuTimes,"getDdSubproblemCpuTimes"),
+		    (:getDdSubproblemWallTimes,"getDdSubproblemWallTimes"),
+		    (:getDdSubproblemPrimalBounds,"getDdSubproblemPrimalBounds"),
+		    (:getDdSubproblemDualBounds,"getDdSubproblemDualBounds")]
+	@eval begin
+		function $func()
+			size = getDdNumSubproblemSolved()
+			val = Array(Cdouble, size)
+			@dsp_ccall($name, Void, (Ptr{Void}, Ptr{Cdouble}), env.p, val)
+			return val
+		end
+	end
 end
-
-function getDdSubprobTime()
-	num = @dsp_ccall("getDdSubprobTimeSize", Cint, (Ptr{Void},), env.p)
-	time = Array(Cdouble, num)
-	@dsp_ccall("getDdSubprobTime", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, time)
-	return time
-end
-
-function getDdMasterObjValues()
-	num = @dsp_ccall("getDdNumMasterObjValues", Cint, (Ptr{Void},), env.p)
-	vals = Array(Cdouble, num)
-	@dsp_ccall("getDdMasterObjValues", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
-	return vals
-end
-
-function getDdSubPrimalBounds()
-	num = @dsp_ccall("getDdNumSubPrimalBounds", Cint, (Ptr{Void},), env.p)
-	vals = Array(Cdouble, num)
-	@dsp_ccall("getDdSubPrimalBounds", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
-	return vals
-end
-
-function getDdSubproblemObjValues()
-	return getDdSubPrimalBounds()
-end
-
-function getDdSubDualBounds()
-	num = @dsp_ccall("getDdNumSubDualBounds", Cint, (Ptr{Void},), env.p)
-	vals = Array(Cdouble, num)
-	@dsp_ccall("getDdSubDualBounds", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
-	return vals
-end
-
-function getDdPrimalBounds()
-	num = @dsp_ccall("getDdNumPrimalBounds", Cint, (Ptr{Void},), env.p)
-	vals = Array(Cdouble, num)
-	@dsp_ccall("getDdPrimalBounds", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
-	return vals
-end
-
-function getDdDualBounds()
-	num = @dsp_ccall("getDdNumDualBounds", Cint, (Ptr{Void},), env.p)
-	vals = Array(Cdouble, num)
-	@dsp_ccall("getDdDualBounds", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
-	return vals
-end
-
-function getDdCpuTime()
-	return @dsp_ccall("getDdCpuTime", Cdouble, (Ptr{Void},), env.p)
-end
-
-function getDdNumChangesOfMultiplier()
-	return @dsp_ccall("getDdNumChangesOfMultiplier", Cint, (Ptr{Void},), env.p)
-end
-
-function getDdChangesOfMultiplier()
-	num = getDdNumChangesOfMultiplier()
-	changes = Array(Cdouble, num);
-	@dsp_ccall("getDdChangesOfMultiplier", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, changes)
-	return changes
-end
-
