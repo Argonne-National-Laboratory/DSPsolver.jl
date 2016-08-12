94faad4bee4dd513460952f229268f4de83c6632
Kibaek Kim <kibaek.k@gmail.com>
2016-08-12 15:15:58 -0500

    add more functions and cleanup

--- src/get_dd.jl
+++ src/get_dd.jl
@@ -1,7 +1,3 @@
-function getDdNumInfeasSolutions()
-	return @dsp_ccall("getDdNumInfeasSolutions", Cint, (Ptr{Void},), env.p)
-end
-
 function getDdIterTime()
 	num = @dsp_ccall("getDdIterTimeSize", Cint, (Ptr{Void},), env.p)
 	time = Array(Cdouble, num)
@@ -30,10 +26,21 @@ function getDdMasterObjValues()
 	return vals
 end
 
-function getDdSubproblemObjValues()
-	num = @dsp_ccall("getDdNumSubproblemObjValues", Cint, (Ptr{Void},), env.p)
+function getDdSubPrimalBounds()
+	num = @dsp_ccall("getDdNumSubPrimalBounds", Cint, (Ptr{Void},), env.p)
 	vals = Array(Cdouble, num)
-	@dsp_ccall("getDdSubproblemObjValues", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
+	@dsp_ccall("getDdSubPrimalBounds", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
+	return vals
+end
+
+function getDdSubproblemObjValues()
+	return getDdSubPrimalBounds()
+end
+
+function getDdSubDualBounds()
+	num = @dsp_ccall("getDdNumSubDualBounds", Cint, (Ptr{Void},), env.p)
+	vals = Array(Cdouble, num)
+	@dsp_ccall("getDdSubDualBounds", Void, (Ptr{Void}, Ptr{Cdouble}), env.p, vals)
 	return vals
 end
 
@@ -51,14 +58,6 @@ function getDdDualBounds()
 	return vals
 end
 
-function getDdCpuTime()
-	return @dsp_ccall("getDdCpuTime", Cdouble, (Ptr{Void},), env.p)
-end
-
-function getDdNumChangesOfMultiplier()
-	return @dsp_ccall("getDdNumChangesOfMultiplier", Cint, (Ptr{Void},), env.p)
-end
-
 function getDdChangesOfMultiplier()
 	num = getDdNumChangesOfMultiplier()
 	changes = Array(Cdouble, num);
@@ -66,3 +65,17 @@ function getDdChangesOfMultiplier()
 	return changes
 end
 
+for (func,rtn) in [(:getDdNumInfeasSolutions, Cint),
+		   (:getDdCpuTime, Cdouble),
+                   (:getDdNumChangesOfMultiplier, Cint),
+                   (:getDdMasterTotalTime, Cdouble),
+                   (:getDdLbTotalTime, Cdouble),
+                   (:getDdUbTotalTime, Cdouble),
+                   (:getDdCgTotalTime, Cdouble)]
+	strfunc = string(func)
+	@eval begin
+		function $func()
+			return @dsp_ccall($strfunc, $rtn, (Ptr{Void},), env.p)
+		end
+	end
+end
